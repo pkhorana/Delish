2733223ec87642938f5daf49da73ae6c121ed08c
Elton <eltonp3103@gmail.com>
2019-07-29 11:20:15 -0400

    Revert "image capturing ability done"
    This reverts commit e3386dbc7671e2f2d7d0cc75ff7b0ed66322b665.

--- app/src/main/java/com/example/delish/MainActivity.java
+++ app/src/main/java/com/example/delish/MainActivity.java
@@ -1,49 +1,14 @@
 package com.example.delish;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
 
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button takeAPicture = findViewById(R.id.take_picture_button);
-        takeAPicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dispatchTakePictureIntent();
-            }
-        });
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takeAPicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takeAPicture.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takeAPicture, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            processImage(imageBitmap);
-        }
-    }
-
-    public void processImage(Bitmap imageBitMap) {
-        // code that in
     }
 }
