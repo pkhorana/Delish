ac5761c4aba62b21f25932d292a6258f5ec9b5cc
Elton <eltonp3103@gmail.com>
2019-07-31 17:16:29 -0400

    merge commit. made activity and xml page

---
+++ app/src/main/java/com/example/delish/View/NutritionActivity.java
@@ -0,0 +1,16 @@
+package com.example.delish.View;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.delish.R;
+
+public class NutritionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nutrient_insights_page);
+    }
+}
