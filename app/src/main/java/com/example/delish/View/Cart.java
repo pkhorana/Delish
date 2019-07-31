3071de00bca94edf62c6e859bf4618b017c2961a
RLBlaze77 <rli342@gatech.edu>
2019-07-31 12:26:29 -0400

    merge commit

--- app/src/main/java/com/example/delish/View/Cart.java
+++ app/src/main/java/com/example/delish/View/Cart.java
@@ -1,5 +1,7 @@
 package com.example.delish.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
@@ -12,14 +14,35 @@ import android.widget.Toast;
 import com.example.delish.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-public class Cart extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart extends AppCompatActivity implements CartAdapter.ItemClickListener {
     private float x1,x2; //USED IN SWIPE DETECTION
     static final int MIN_DISTANCE = 150; //USED IN SWIPE DETECTION
+    CartAdapter adapter;
+    List<String> cart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.cart_holder_view);
+
+        cart.add("cheese");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new CartAdapter(this, cart);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
---
+++ app/src/main/java/com/example/delish/View/CartAdapter.java
@@ -0,0 +1,76 @@
+package com.example.delish.View;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public CartAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recyclerview_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String food = mData.get(position);
+        holder.itemName.setText(food);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    // convenience method for getting data at click position
+    public int getItemCount(int id) {
+        if(mData!=null){
+            return mData.size();
+        }
+        return 0;
+    }
+
+    public String getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView itemName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemName = (TextView) itemView.findViewById(R.id.itemName);
+
+        }
+    }
+}
